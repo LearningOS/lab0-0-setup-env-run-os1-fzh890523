09486a3a6d66d89d3d79373cbbf0698f416316d2
yonka <252941898@qq.com>
2022-12-11 23:10:47 +0800

    ch1 can boot and exit

--- os/src/main.rs
+++ os/src/main.rs
@@ -33,9 +33,17 @@ pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
     syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
 }
 
-#[no_mangle]
-extern "C" fn _start() {
-  sbi::shutdown();
-}
+// #[no_mangle]
+// extern "C" fn _start() {
+//   sbi::shutdown();
+// }
+// 因为下面汇编里定义了 _start 并且call rust_main，所以不能重复，否则会
+// error: symbol '_start' is already defined
 
+core::arch::global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    sbi::shutdown();
+}
 
